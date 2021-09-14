ea7f9bbf4f1a56422abf1ef80ead5f33ca89d133
michael <michaelsam00@yahoo.com>
2021-09-14 14:48:40 +0200

    -add travis file

--- app/src/test/java/com/example/moviedb/data/FakeMovieRemoteDataSource.kt
+++ app/src/test/java/com/example/moviedb/data/FakeMovieRemoteDataSource.kt
@@ -4,6 +4,7 @@ import com.example.moviedb.Dates
 import com.example.moviedb.NowPlayingRes
 import com.example.moviedb.Result
 import com.example.moviedb.data.network.NetworkCallback
+import java.lang.Exception
 
 class FakeMovieRemoteDataSource : MovieDataSource {
 
@@ -38,6 +39,11 @@ class FakeMovieRemoteDataSource : MovieDataSource {
         }
     }
 
+    override suspend fun getMoviesCoroutine(): com.example.moviedb.data.Result<NowPlayingRes> {
+        initMovies()
+        return if(shouldReturnNetworkError()) com.example.moviedb.data.Result.Error(Exception())
+        else com.example.moviedb.data.Result.Success(movieRes)
+    }
 
 
 }
